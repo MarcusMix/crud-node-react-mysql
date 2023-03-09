e5ddb55addb2e7ce4c00f9c7ae6290a4dd0118ca
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-09 13:14:32 -0300

    style: add text e box shadow

--- client/src/components/card/card.styles.ts
+++ client/src/components/card/card.styles.ts
@@ -10,6 +10,7 @@ export const CardStyled = styled.div`
     cursor: pointer;
     border-radius: 0.7rem;
     min-width: 650px;
+    box-shadow: rgba(0, 0, 0, 0.30) 2.4px 2.4px 3.2px;
     
     :hover {
         border: 4px solid #FF4858;
@@ -19,11 +20,13 @@ export const CardStyled = styled.div`
     & h1 {
         font-size: 2.5rem;
         color: #8b8a8b;
+        text-shadow: #001 2.4px 2.4px 3.2px;
     }
     
     & p {
         font-size: 2rem;
         padding: 1rem;
         color: #8b8a8b;
+        text-shadow: #001 2.4px 2.4px 3.2px;
     }
 `
